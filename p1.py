9a3760dafb5ad0c0199269968fa1eb0a5516c2fb
Deepak Singh <ideepak.4190@gmail.com>
2025-06-29 10:21:27 +0530

    Created using Colab

--- p1.py
+++ p1.py
@@ -4,7 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyOtQoTkBwKdAd4YgVyzTRpq",
+      "authorship_tag": "ABX9TyOmozY34rMEwkpOkVg3yx3O",
       "include_colab_link": true
     },
     "kernelspec": {
@@ -47,6 +47,39 @@
       ],
       "source": [
         "print(\"hello World\")"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "rows= 5\n",
+        "for i in range (1,rows+1):\n",
+        "  for space in range(rows-i):\n",
+        "    print(\" \",end=\" \")\n",
+        "  for star in range(i):\n",
+        "    print(\"*\",end=\" \")\n",
+        "  print()"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "eneeWkoSuUjC",
+        "outputId": "32d23388-650e-424d-82b1-c54c2a777fc0"
+      },
+      "execution_count": 4,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "        * \n",
+            "      * * \n",
+            "    * * * \n",
+            "  * * * * \n",
+            "* * * * * \n"
+          ]
+        }
       ]
     }
   ]
